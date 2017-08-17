318d0aa1acf3e7a7267592d510630fc4225b05d0
Matthew Leon <ml@matthewleon.com>
2017-08-17 22:12:52 +0100

    parse log lines

--- app/Git/Log.hs
+++ app/Git/Log.hs
@@ -1,17 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Git.Log (gitLogOneline) where
 
+import Control.Arrow ((>>>))
+import Control.Exception.Safe (throwString)
+import Data.Maybe (maybe)
 import qualified Data.Text as T
 import Git.Command (Subcommand(..), Flag(..), git)
+import Git.Types.SHA (SHA)
+import qualified Git.Types.SHA as SHA
 import Turtle
 
-gitLogOneline :: Shell Line
-gitLogOneline = gitLog [Oneline]
+gitLogOneline :: Shell (SHA, Text)
+gitLogOneline = parseLine =<< gitLogOneline'
+  where
+  parseLine :: Line -> Shell (SHA, Text)
+  parseLine =
+        lineToText
+    >>> \t ->
+      case match logPattern t of
+        [(shaText, comment)] ->
+          maybe
+          (throwText $ "Failed to parse SHA: " <> shaText)
+          (\sha -> return (sha, comment))
+          $ SHA.fromText shaText
+        _ -> throwText $ "Failed to parse log line: " <> t
+    where
+    logPattern = (,) <$> (sha <* char ' ') <*> chars
+      where sha = plus hexDigit
+    throwText = liftIO . throwString . T.unpack
+
+gitLogOneline' :: Shell Line
+gitLogOneline' = gitLog [Oneline]
 
 data LogFlag = Oneline
   deriving (Show)
 
 logFlagToFlag :: LogFlag -> Flag
-logFlagToFlag = Flag . T.pack . show
+logFlagToFlag Oneline = Flag "pretty=oneline"
 
 gitLog :: [LogFlag] -> Shell Line
 gitLog = git Log . fmap logFlagToFlag
--- app/Git/Types/SHA.hs
+++ app/Git/Types/SHA.hs
@@ -6,21 +6,21 @@ module Git.Types.SHA (
 , toText
 ) where
 
-import           Control.Monad.Fail (MonadFail(fail))
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Base16 as B16
+import           Data.Maybe (Maybe(Just, Nothing))
 import           Data.Text (Text)
 import           Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import           Prelude hiding (fail)
 
 newtype SHA = SHA ByteString
-  deriving (Eq, Ord, Read)
+  deriving (Eq, Ord, Show)
 
 toText :: SHA -> Text
 toText (SHA bs) = decodeUtf8 $ B16.encode bs
 
-fromText :: MonadFail m => Text -> m SHA
+fromText :: Text -> Maybe SHA
 fromText t =
     case B16.decode $ encodeUtf8 t of
-        (bs, "") -> return $ SHA bs
-        _        -> fail "Invalid base16 encoding"
+        (bs, "") -> Just $ SHA bs
+        _        -> Nothing
